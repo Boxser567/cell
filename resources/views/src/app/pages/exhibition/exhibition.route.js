851e14f06c59b043bc2dea7e1ae95d65fda3df2d
shd <shd@gokuai.cn>
2016-08-22 10:35:28 +0800

    router

--- resources/views/src/app/pages/exhibition/exhibition.route.js
+++ resources/views/src/app/pages/exhibition/exhibition.route.js
@@ -5,25 +5,25 @@ import exhibitionListTpl from './exhibition-list.html';
 import exhibitionDetailTpl from './exhibition-detail.html';
 
 function routeConfig($stateProvider) {
-  'ngInject';
+    'ngInject';
 
-  $stateProvider
-    .state('main.exhibition-list', {
-      url: '^/exhibition',
-      templateUrl: exhibitionListTpl,
-      controller: require('./exhibition-list.controller')
-    })
-    .state('main.exhibition-detail', {
-      url: '^/exhibition/:id',
-      templateUrl: exhibitionDetailTpl,
-      controller: require('./exhibition-detail.controller'),
-      resolve: {
-        currentExhibition: ['Exhibition', '$stateParams', function(Exhibition, $stateParams) {
-          return Exhibition.getById(parseInt($stateParams.id));
-        }]
-      }
-    });;
+    $stateProvider
+        .state('main.exhibition-list', {
+            url: '^/exhibition',
+            templateUrl: exhibitionListTpl,
+            controller: require('./exhibition-list.controller')
+        })
+        .state('main.exhibition-detail', {
+            url: '^/exhibition/:id',
+            templateUrl: exhibitionDetailTpl,
+            controller: require('./exhibition-detail.controller'),
+            resolve: {
+                currentExhibition: ['Exhibition', '$stateParams', function (Exhibition, $stateParams) {
+                    return Exhibition.getById(parseInt($stateParams.id));
+                }]
+            }
+        });
 
-}``
+}
 
 export default routeConfig;
