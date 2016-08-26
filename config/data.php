a8626f1f2542e64c018c54b5f008b746966dd850
Yingxiangempire <yingxiangempire@outlook.com>
2016-08-26 10:03:42 +0800

    创建

--- config/data.php
+++ config/data.php
@@ -6,6 +6,6 @@
  * Time: 下午4:41
  */
 return[
-    'LOGO'=>['aaaa.jpeg','15-080848_130.jpg'],
+    'LOGO'=>['aaaa.jpeg','15-080848_130.jpg','aaaa.jpeg','15-080848_130.jpg'],
     'BANNER'=>['aaaa.jpeg','15-080848_130.jpg','aaaa.jpeg','15-080848_130.jpg']
 ];
