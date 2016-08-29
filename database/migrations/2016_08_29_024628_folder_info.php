1238cb3d529ce51605fe1cf503ca40141964044f
Yingxiangempire <yingxiangempire@outlook.com>
2016-08-29 17:42:55 +0800

    migration

--- database/migrations/2016_08_29_024628_folder_info.php
+++ database/migrations/2016_08_29_024628_folder_info.php
@@ -1,15 +1,15 @@
 <?php class FolderInfo extends CreateBase
 {
     protected $table = "folder_info";
-    protected $connection = "yunku_boss";
 
     public function create(&$table)
     {
+        $table->increments('id');
         $table->integer("org_id");
         $table->string("folder_hash", 30);
-        $table->interger("file_count");
-        $table->interger("file_size");
-        $table->dataTime("created_at");
-        $table->dataTime("updated_at");
+        $table->integer("file_count");
+        $table->integer("file_size");
+        $table->dateTime("created_at");
+        $table->dateTime("updated_at");
     }
 }
