46a729e480270100704b4c02365a989b38b5f0ed
shd <shd@gokuai.cn>
2016-09-07 10:45:01 +0800

    buil;d

--- resources/views/src/app/components/cell-header/cell-header.directive.js
+++ resources/views/src/app/components/cell-header/cell-header.directive.js
@@ -9,11 +9,18 @@ function cellHeaderComponent($log) {
         restrict: 'E',
         templateUrl: cellHeaderTpl,
         controllerAs: 'vm',
+        controller: cellHeaderController,
         bindToController: true
     };
 
     return directive;
 
+    function cellHeaderController($scope, $rootScope, Exhibition, localStorageService) {
+        'ngInject';
+        if (!$rootScope.user) {
+            $rootScope.user = localStorageService.get('user');
+        }
+    }
 }
 
 export default cellHeaderComponent;
--- resources/views/src/app/pages/login/login.controller.js
+++ resources/views/src/app/pages/login/login.controller.js
@@ -6,21 +6,15 @@ function LoginController($scope, $rootScope, Exhibition, localStorageService) {
     $rootScope.projectTitle = "会文件";
     // Login.getLoginInfo();
     $scope.loginIn = function () {
-        // var user = localStorageService.get('user');
-        // if (!user) {
         $rootScope.alertMsg = false;
         Exhibition.login().then(function (res) {
             var user = {
                 loginName: res.name
             };
-            //localStorageService.set('user', user);
+            localStorageService.set('user', user);
             $rootScope.user = user;
             window.location.href = "/#/exhibition";
         })
-        //} else {
-        //     $rootScope.user = user;
-        //}
-
     }
 
 }
