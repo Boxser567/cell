3bd6324642c8efc1b48f1aedbb1b9f2dfcde4416
Yingxiangempire <yingxiangempire@outlook.com>
2016-09-18 17:27:50 +0800

    更新

--- database/migrations/2016_08_15_062419_member.php
+++ database/migrations/2016_08_15_062419_member.php
@@ -7,8 +7,8 @@ class Member extends CreateBase
     {
         $table->increments('id');
         $table->string("name",100);
-        $table->string("image",100);
-        $table->integer("unionid");
+        $table->string("image",200);
+        $table->string("unionid",50);
         $table->integer("ent_id");
         $table->dateTime("created_at");
         $table->dateTime("updated_at");
