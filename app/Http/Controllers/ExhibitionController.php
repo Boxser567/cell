ba28be1bf2587ec9fbcc61611690f8880a54e423
Yingxiangempire <yingxiangempire@outlook.com>
2016-09-29 14:10:52 +0800

    版本配置&删除分组

--- app/Http/Controllers/ExhibitionController.php
+++ app/Http/Controllers/ExhibitionController.php
@@ -19,7 +19,7 @@ use Log;
 class ExhibitionController extends BaseController
 {
     const PRE_FIX = "Exhibition_";
-    const BASE_FILE_NAME='base_folder';
+    const BASE_FILE_NAME = 'base_folder';
     private $ent_id;
 
     public function __construct()
@@ -42,15 +42,16 @@ class ExhibitionController extends BaseController
     //创建展会
     public function postCreate()
     {
-        $ex_count=ExhibitionInfo::getCountByEntId($this->ent_id);
-        $meanwhile_ex_count=ExhibitionInfo::getCountByEntTime(date('Y-m-d'),$this->ent_id);
-      //  $this->judgePermission('meanwhile_amount',$meanwhile_ex_count);//权限判断同时进行的会展
-      //  $this->judgePermission('exhi_amount',$ex_count);//权限判断会展总数量
+        $ex_count = ExhibitionInfo::getCountByEntId($this->ent_id);
+        $meanwhile_ex_count = ExhibitionInfo::getCountByEntTime(date('Y-m-d'), $this->ent_id);
+        //  $this->judgePermission('meanwhile_amount',$meanwhile_ex_count);//权限判断同时进行的会展
+        //  $this->judgePermission('exhi_amount',$ex_count);//权限判断会展总数量
         $yunku_org = new YunkuOrg();
-        $org_size=$this->member['edition_info']['ex_size']==-1?"":$this->member['edition_info']['ex_size']*1024*1024;//权限设置单个会展可用空间
-        $org = $yunku_org->setOrg(self::PRE_FIX . $this->member['id'],'',$org_size);
-        $yunku_file=new YunkuFile($org['org_id']);
-       //$yunku_file->setFolder(self::BASE_FILE_NAME);
+        // $org_size=$this->member['edition_info']['ex_size']==-1?"":$this->member['edition_info']['ex_size']*1024*1024;//权限设置单个会展可用空间
+        $org = $yunku_org->setOrg(self::PRE_FIX . $this->member['id']);
+        // $org = $yunku_org->setOrg(self::PRE_FIX . $this->member['id'],'',$org_size);
+        $yunku_file = new YunkuFile($org['org_id']);
+        //$yunku_file->setFolder(self::BASE_FILE_NAME);
         $exhibition = LAccount::setExhibition($this->ent_id, $org['org_id']);
         $group = LAccount::setGroup($exhibition->id);
         $new_exhibition = ExhibitionInfo::getOfOrgId($exhibition->org_id);
@@ -83,9 +84,17 @@ class ExhibitionController extends BaseController
     //创建新分组
     public function postGroup()
     {
-        $group_count=GroupInfo::getCountByExId(inputGetOrFail('ex_id'));
-       // $this->judgePermission('group_count',$group_count);//权限判断分组个数
+        $group_count = GroupInfo::getCountByExId(inputGetOrFail('ex_id'));
+        // $this->judgePermission('group_count',$group_count);//权限判断分组个数
         return LAccount::setGroup(inputGetOrFail('ex_id'));
+    }
+
+    //删除分组
+    public function postDeleteGroup()
+    {
+        GroupInfo::deleteById(inputGetOrFail('group_id'));
+        GroupInfo::cacheForget();
+        return;
     }
 
     //更新分组信息
@@ -97,7 +106,7 @@ class ExhibitionController extends BaseController
         }
         if (inputGet('name')) {
             //$this->judgePermission('name_group');//权限判断自定义分组名称
-            $group_info->name= inputGet('name');
+            $group_info->name = inputGet('name');
         }
         if (inputGet('start_time')) {
             $group_info->start_time = inputGet('start_time');
@@ -113,7 +122,7 @@ class ExhibitionController extends BaseController
             $folder_info = FolderInfo::getByGroupId(inputGetOrFail('group_id'))->toArray();
             foreach ($folder_info as $folder) {
                 $base_folder = FolderInfo::_findOrFail($folder['id']);
-                if (inputGet('start_time') && $base_folder->start_time <inputGet('start_time')) {
+                if (inputGet('start_time') && $base_folder->start_time < inputGet('start_time')) {
                     $base_folder->start_time = inputGet('start_time');
                 }
                 if (inputGet('end_time') && $base_folder->end_time > inputGet('end_time')) {
@@ -160,25 +169,25 @@ class ExhibitionController extends BaseController
             $exhibition->title = inputGet('title');
         }
         if (inputGet('logo')) {
-           // $this->judgePermission('design_logo');//权限判断自定义会展详情及 Logo
+            // $this->judgePermission('design_logo');//权限判断自定义会展详情及 Logo
             $exhibition->logo = inputGet('logo');
         }
         if (inputGet('banner')) {
-         //   $this->judgePermission('upload_banner');//权限判断上传自定义 Banner 背景
+            //   $this->judgePermission('upload_banner');//权限判断上传自定义 Banner 背景
             $exhibition->banner = inputGet('banner');
         }
         if (inputGet('start_date')) {
-            $before_hour=strtotime(strtotime(inputGet('start_date'))-time())/(60*60);
-          //  $this->judgePermission('end_date',$before_hour);//权限判断展会筹备时间
+            $before_hour = strtotime(strtotime(inputGet('start_date')) - time()) / (60 * 60);
+            //  $this->judgePermission('end_date',$before_hour);//权限判断展会筹备时间
             $exhibition->start_date = inputGet('start_date');
         }
         if (inputGet('end_date')) {
-            $last_hour=(strtotime(inputGet('end_date'))-strtotime(inputGet('start_date')))/(60*60);
-           // $this->judgePermission('meeting_last_hour',$last_hour);//权限判断会展持续时间
+            $last_hour = (strtotime(inputGet('end_date')) - strtotime(inputGet('start_date'))) / (60 * 60);
+            // $this->judgePermission('meeting_last_hour',$last_hour);//权限判断会展持续时间
             $exhibition->end_date = inputGet('end_date');
         }
         if (inputGet('website')) {
-          //  $this->judgePermission('web_link');
+            //  $this->judgePermission('web_link');
             $exhibition->property = json_encode(["web_site" => inputGet('website')]);
         }
         $exhibition->save();
@@ -188,7 +197,7 @@ class ExhibitionController extends BaseController
     //开启/关闭资料收集
     public function postResCollection()
     {
-       // $this->judgePermission('res_collection');//权限判断资料收集开启或者关闭
+        // $this->judgePermission('res_collection');//权限判断资料收集开启或者关闭
         $exhibition = ExhibitionInfo::_findOrFail(inputGetOrFail('exhibition_id'));
         $action = inputGetOrFail('action');
         switch ($action) {
@@ -201,7 +210,7 @@ class ExhibitionController extends BaseController
                 $exhibition->res_collect_lock = -1;
                 break;
             default:
-                throw new \Exception("参数错误",400001);
+                throw new \Exception("参数错误", 400001);
         }
         $exhibition->save();
         ExhibitionInfo::cacheForget();
@@ -215,7 +224,7 @@ class ExhibitionController extends BaseController
     {
         $exhibition = $exhibition->toArray();
         $exhibition['unique_code'] = "http://" . config("app.view_domain") . "/#/mobile/" . $exhibition['unique_code'];
-        $exhibition['base_folder']=self::BASE_FILE_NAME;
+        $exhibition['base_folder'] = self::BASE_FILE_NAME;
         if ($exhibition['res_collect_lock'] != 0) {
             $exhibition['res_collect'] = FileController::RES_COLLECTION_FOLDER_NAME;
         }
